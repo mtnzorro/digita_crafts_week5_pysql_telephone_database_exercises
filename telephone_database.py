c571504a340e1de31fd9d345233851aa7c88bd59
Jason Campbell <jrcampbell78@gmail.com>
2016-10-11 13:04:52 -0400

    added definitions and updated logic for querying database

--- telephone_database.py
+++ telephone_database.py
@@ -2,14 +2,68 @@ import pg
 
 db = pg.DB(dbname='electronic_phone_book')
 
+def lookup():
+    found_name = False
+    name_look_up = raw_input("Name: ").lower().title()
+    # for names in name_results:
+    result_list = db.query("select * from phonebook where name ilike '%s'" % name_look_up).namedresult()
+    if len(result_list)>0:
+        for names in result_list:
+            print "Found entry for %s: Home: %s, Cell: %s, Work: %s, Email: %s" % (names.name, names.phone_number, names.cell_number, names.work_number, names.email)
+    #     if names.name == name_look_up:
+    #         print "Found entry for %s: Home: %s, Cell: %s, Work: %s, Email: %s" % (names.name, names.phone_number, names.cell_number, names.work_number, names.email)
+    #         found_name = True
+    #         break
+    #     else:
+    #         pass
+    # if found_name == False:
+    #     print "No entry found for %s" % name_look_up
+    # found_name = False
+    else:
+        print "No entry found for %s" % name_look_up
+def set_entry():
+    name_set = raw_input("Name: ").lower().title()
+    which_number =  raw_input("Home phone number: ")
+    which_cell_number =  raw_input("Cell phone number: ")
+    which_work_number =  raw_input("Work phone number: ")
+    email_input = raw_input("Email: ")
+    result_list = db.query("select id from phonebook where name ilike '%s'" % name_set).namedresult()
+    if len(result_list) > 0:
+        id = result_list[0].id
+        db.update('phonebook', {
+            'id' :id,
+            'name' : name_set,
+            'phone_number' : which_number,
+            'cell_number' : which_cell_number,
+            'work_number' : which_work_number,
+            'email': email_input
+        })
+        print "Entry updated for %s" % name_set
+    else:
+        db.insert('phonebook', name = name_set, phone_number = which_number, cell_number = which_cell_number, work_number = which_work_number, email = email_input)
+        print "Entry stored for %s" % name_set
 
-# CREATE TABLE phonebook (
-#   id serial PRIMARY KEY,
-#   name varchar NOT NULL UNIQUE,
-#   phone_number varchar,
-#   email varchar UNIQUE
-# );
-found_name = False
+def delete_entry():
+    name_del = raw_input("Name: ").lower().title()
+    found_name = False
+    for names in name_results:
+        if names.name == name_del:
+            print "Deleted entry for %s" % names.name
+            db.delete('phonebook', {'id' : names.id})
+            found_name = True
+            break
+        else:
+            pass
+    if found_name == False:
+        print "No entry found for %s" % name_del
+    found_name = False
+
+def list_all():
+    for names in name_results:
+        print "Found entry for %s : Phone Number: %s, Cell Number: %s, Work Number: %s, Email: %s" % (names.name, names.phone_number, names.cell_number, names.work_number, names.email)
+
+
+
 x = 0
 while (x == 0):
     name_query = db.query('select * from phonebook')
@@ -25,81 +79,16 @@ while (x == 0):
     menu_select = int(raw_input("What do you want to do (1-5)? "))
 
     if menu_select == 1:
-        name_look_up = raw_input("Name: ").lower().title()
-
-        for names in name_results:
-            if names.name == name_look_up:
-                print "Found entry for %s: Home: %s, Cell: %s, Work: %s, Email: %s" % (names.name, names.phone_number, names.cell_number, names.work_number, names.email)
-                found_name = True
-                break
-            else:
-                pass
-        if found_name == False:
-            print "No entry found for %s" % name_look_up
-        found_name = False
+        lookup()
 
     if menu_select == 2:
-        name_set = raw_input("Name: ").lower().title()
-        which_number =  raw_input("Home phone number: ")
-        which_cell_number =  raw_input("Cell phone number: ")
-        which_work_number =  raw_input("Work phone number: ")
-        email_input = raw_input("Email: ")
-        db.insert('phonebook', name = name_set, phone_number = which_number, cell_number = which_cell_number, work_number = which_work_number, email = email_input)
-        print "Entry stored for %s" % name_set
+        set_entry()
 
     if menu_select == 3:
-        name_del = raw_input("Name: ").lower().title()
+        delete_entry()
 
-        for names in name_results:
-            if names.name == name_del:
-                print "Deleted entry for %s" % names.name
-                db.delete('phonebook', {'id' : names.id})
-                found_name = True
-                break
-            else:
-                pass
-        if found_name == False:
-            print "No entry found for %s" % name_del
-        found_name = False
-
-
-
-    #
-    #     else:
-    #         temp_dict[name_set] = {}
-    #
-    #     if which_number == "cell":
-    #         cell_number = raw_input("Enter cell number: ")
-    #         temp_dict[name_set]['cell'] = cell_number
-    #     elif which_number == "work":
-    #         work_number = raw_input("Enter work number: ")
-    #         temp_dict[name_set]['work'] = work_number
-    #     elif which_number == "home":
-    #         home_number = raw_input("Enter home number: ")
-    #         temp_dict[name_set]['home'] = home_number
-    #     else:
-    #         pass
-    #     name_set = name_set.title()
-    #     print "Entry stored for %s" % name_set
-    #
-    # if menu_select == 3:
-    #     name_look_up = raw_input("Name: ").lower()
-    #     name_find = name_look_up in temp_dict
-    #     if name_find:
-    #         del temp_dict[name_look_up]
-    #         print "Deleted entry for %s" % name_look_up
-    #     else:
-    #         print "No entry found for %s" %name_look_up
     if menu_select == 4:
-            for names in name_results:
-                print "Found entry for %s : Phone Number: %s, Cell Number: %s, Work Number: %s, Email: %s" % (names.name, names.phone_number, names.cell_number, names.work_number, names.email)
-
-    #     #print_out = temp_dict.items()
-    #     for names, tels in temp_dict.iteritems():
-    #         cell_number = tels.get("cell", "N/A")
-    #         home_number = tels.get("home", "N/A")
-    #         work_number = tels.get("work", "N/A")
-    #         print "Found entry for %s : Cell number: %s, Home number: %s, Work number: %s" % (names, cell_number, home_number, work_number)
+        list_all()
 
     if menu_select == 5:
         print "Bye."
